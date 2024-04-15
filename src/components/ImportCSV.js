882a08d07acfea232815fccbe09b5b4323f6591d
Bahugunajii <amanbahuguna009@gmail.com>
2024-04-15 18:35:14 +0530

    minor changes

--- src/components/ImportCSV.js
+++ src/components/ImportCSV.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import CSVReader from 'react-csv-reader';
 import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS } from 'chart.js/auto';
 
 const CsvReaderComponent = () => {
   const [csvData, setCsvData] = useState([]);
